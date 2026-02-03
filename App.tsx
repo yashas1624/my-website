7aedcbf2291389af65386e992d1d73790258a134
yashas1624 <yashas.study77@gmail.com>
2026-02-03 22:26:16 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import {
 } from 'recharts';
 import { ICONS, SYSTEM_ARCHITECTURE, MVP_ROADMAP } from './constants';
 import { UserProfile, NutritionReport } from './types';
-import { analyzeFoodImage } from './services/geminiService';
+import { analyzeFoodImage } from "./services/GeminiService";
 
 const App: React.FC = () => {
   const [activeTab, setActiveTab] = useState<'dash' | 'scan' | 'design'>('dash');
